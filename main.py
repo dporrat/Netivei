7d2b7edbfdc40024ef6c10c263312f62b718c7b7
Dana Porrat <dporrat@protonmail.com>
2022-01-12 12:48:43 +0200

    collects images

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
 import pyautogui
 import datetime
 import time
+import os
 
 
 if __name__ == '__main__':
     SCREEN_SIZE = (1920, 1080)
     VIDEO_PATH = f'C:\\Users\\dporrat\\Desktop\\Netivei\\videos'
-
-    SCREEN_SIZE = (1280, 1024)
-    VIDEO_PATH = '/media/dana/second local disk1/dana/Netivei/videos'
+    if not os.path.exists(VIDEO_PATH):
+        SCREEN_SIZE = (1280, 1024)
+        VIDEO_PATH = '/media/dana/second local disk1/dana/Netivei/videos'
 
     SECTION_LENGTH_SEC = 60
-    FRAMES_PER_SEC = 10
+    TIME_BETWEEN_FRAMES_SEC = 0.2
 
-    while True:
-        section_start_time_sec = time.time()
-        fource = cv2.VideoWriter_fourcc(*'XVID')
+    if 0:
+        screenshot = pyautogui.screenshot()
+        screenshot.save(f'{VIDEO_PATH}/filename.png')
 
-        now_utc = datetime.datetime.now(datetime.timezone.utc)
-        now_str = now_utc.strftime('%Y_%m_%d_%H_%M_%S')
-        filename = f'{VIDEO_PATH}\\capture_{now_str}.mp4'
-        out = cv2.VideoWriter(filename, fource, 20.0, (SCREEN_SIZE))
-
+    if 1:  # save images
         prev_time_sec = time.time()
-        while time.time() - section_start_time_sec <= SECTION_LENGTH_SEC:
+        while True:
             time_elapsed_sec = time.time() - prev_time_sec
             img = pyautogui.screenshot()
-            if time_elapsed_sec > 1.0 / FRAMES_PER_SEC:
-                prev_time_sec = time.time()
-                frame = np.array(img)
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                out.write(frame)
-            # cv2.waitKey(1000)
+            if time_elapsed_sec >= TIME_BETWEEN_FRAMES_SEC:
+                now_utc = datetime.datetime.now(datetime.timezone.utc)
+                now_str = now_utc.strftime('%Y_%m_%d_%H_%M_%S_%f')
+                filename = f'{VIDEO_PATH}/capture_{now_str}.png'
+                img.save(filename)
+    else:  # save video
+        while True:
+            section_start_time_sec = time.time()
+            fource = cv2.VideoWriter_fourcc(*'XVID')
+
+            now_utc = datetime.datetime.now(datetime.timezone.utc)
+            now_str = now_utc.strftime('%Y_%m_%d_%H_%M_%S')
+            filename = f'{VIDEO_PATH}\\capture_{now_str}.mp4'
+            out = cv2.VideoWriter(filename, fource, 20.0, (SCREEN_SIZE))
+
+            prev_time_sec = time.time()
+            while time.time() - section_start_time_sec <= SECTION_LENGTH_SEC:
+                time_elapsed_sec = time.time() - prev_time_sec
+                img = pyautogui.screenshot()
+                if time_elapsed_sec > 1.0 / FRAMES_PER_SEC:
+                    prev_time_sec = time.time()
+                    frame = np.array(img)
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    out.write(frame)
+                # cv2.waitKey(1000)
