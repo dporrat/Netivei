b45be6759c91f73327cb3f64ee0d538328b901f2
Dana Porrat <danap@socialmindautism.com>
2022-01-26 13:16:19 +0200

    update

--- collect_video.py
+++ collect_video.py
@@ -1,70 +1,105 @@
-import pyautogui
 import datetime
 import time
 import os
+import shutil
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-from constants import TIME_BETWEEN_FRAMES_SEC, VIDEO_BASEPATH, OS_SEPARATOR, CAMERA, CAMERA_LINKS
+from constants import TIME_BETWEEN_FRAMES_SEC, VIDEO_BASEPATH, OS_SEPARATOR, CAMERAS, CAMERA_URL, VIDEO_RESET_MIN
+
+
+def locate_element(driver_, xpath_string):
+    ii_try = 0
+    while ii_try < 5:
+        try:
+            search_element_ = driver_.find_element(By.XPATH, xpath_string)
+            return search_element_
+        except:
+            ii_try += 1
+            time.sleep(10)
+    return None
 
 
 if __name__ == '__main__':
 
-    # open url - doesn't work
-    if 0:
-        driver = webdriver.Firefox()
-        # driver = webdriver.Chrome()
-        driver.maximize_window()
-        # driver.get("https://www.google.com/")
-        # driver.find_element_by_name("q").send_keys("javatpoint")
+    if 1:
+        # Selenium: https://www.simplilearn.com/tutorials/python-tutorial/selenium-with-python#selenium_webdriver_methods
+        # Selenium: https://pythonspot.com/selenium/
 
-        url = CAMERA_LINKS[CAMERA]
-        driver.get(url)
+        temp_image_name = 'screenshot.png'
+        drivers = []
+        for camera_name in list(CAMERAS.index):
+            drivers.append(webdriver.Firefox())
+            # driver = webdriver.Chrome()
+            drivers[-1].maximize_window()
+            # driver.get("https://www.google.com/")
+            # driver.find_element_by_name("q").send_keys("javatpoint")
 
-        search_element = driver.find_element(By.CLASS_NAME, 'searchBar')
+            url = CAMERA_URL
+            drivers[-1].get(url)
 
-        search_element.click()
-        search_element.send_keys("stam")
 
-    Video_Path = VIDEO_BASEPATH + OS_SEPARATOR + CAMERA
-    if not os.path.exists(Video_Path):
-        os.mkdir(Video_Path)
-    SECTION_LENGTH_SEC = 60
-
-    if 0:
-        screenshot = pyautogui.screenshot()
-        screenshot.save(f'{Video_Path}/filename.png')
-
-    if 1:  # save images
-        prev_time_sec = time.time()
+        start_time = time.time()
         while True:
-            time_elapsed_sec = time.time() - prev_time_sec
-            img = pyautogui.screenshot()
-            if time_elapsed_sec >= TIME_BETWEEN_FRAMES_SEC:
-                now_utc = datetime.datetime.now(datetime.timezone.utc)
-                now_str = now_utc.strftime('%Y_%m_%d_%H_%M_%S_%f')
-                filename = f'{Video_Path}/capture_{now_str}.png'
-                img.save(filename)
-                print(f'saved file {filename}')
-    else:  # save video
-        while True:
-            section_start_time_sec = time.time()
-            fource = cv2.VideoWriter_fourcc(*'XVID')
+            while time.time() < start_time + VIDEO_RESET_MIN * 60:
+                for iiCamera, driver in enumerate(drivers):
+                    camera_name = list(CAMERAS.index)[iiCamera]
+                    driver.refresh()
 
-            now_utc = datetime.datetime.now(datetime.timezone.utc)
-            now_str = now_utc.strftime('%Y_%m_%d_%H_%M_%S')
-            filename = f'{Video_Path}\\capture_{now_str}.mp4'
-            out = cv2.VideoWriter(filename, fource, 20.0, SCREEN_SIZE)
+                    search_element = locate_element(driver, "//input[@class='searchInput']")
+                    if search_element is None:
+                        raise ValueError('Cannot find search link')
+                    search_element.click()
+                    search_element.send_keys(CAMERAS.loc[camera_name, 'search_string'])
 
-            prev_time_sec = time.time()
-            while time.time() - section_start_time_sec <= SECTION_LENGTH_SEC:
-                time_elapsed_sec = time.time() - prev_time_sec
-                img = pyautogui.screenshot()
-                if time_elapsed_sec > 1.0 / FRAMES_PER_SEC:
+                time.sleep(10)
+                for iiCamera, driver in enumerate(drivers):
+                    camera_name = list(CAMERAS.index)[iiCamera]
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2)")
+                    camera_element = locate_element(driver, "//li[@class='col-md-4 cam-item']")
+                    if camera_element is None:
+                        raise ValueError(f'Cannot find camera link for {camera_name}')
+
+                    # camera_element = driver.find_element(By.XPATH, "//li[@class='col-md-4 cam-item']")
+                    camera_element = locate_element(driver, "//li[@class='col-md-4 cam-item']")
+                    if camera_element is None:
+                        raise ValueError('Cannot find camera link')
+                    camera_element.click()
+                    # time.sleep(1)
+
+                time.sleep(3)
+                Video_Paths = []
+                for iiCamera, driver in enumerate(drivers):
+                    camera_name = list(CAMERAS.index)[iiCamera]
+                    try:
+                        modal = driver.find_element(By.CLASS_NAME, 'modal-backdrop fade show')
+                        print(f'{camera_name}: found modal-backdrop')
+                        modal.click()
+                    except:
+                        print(f'{camera_name}: did not find modal-backdrop')
+                        stam = 0
+
+                    Video_Paths.append(VIDEO_BASEPATH + OS_SEPARATOR + camera_name)
+                    if not os.path.exists(Video_Paths[-1]):
+                        os.mkdir(Video_Paths[-1])
+
+                # save images
+                if 1:
                     prev_time_sec = time.time()
-                    frame = np.array(img)
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    out.write(frame)
-                    print(f'saved file {filename}')
-                # cv2.waitKey(1000)
+                    while time.time() < start_time + VIDEO_RESET_MIN * 60:
+                        time_elapsed_sec = time.time() - prev_time_sec
+                        # img = pyautogui.screenshot()
+                        for iiCamera, driver in enumerate(drivers):
+                            img = driver.save_screenshot(temp_image_name)
+                            if time_elapsed_sec >= TIME_BETWEEN_FRAMES_SEC:
+                                now_utc = datetime.datetime.now(datetime.timezone.utc)
+                                now_str = now_utc.strftime('%Y_%m_%d_%H_%M_%S_%f')
+                                filename = f'{Video_Paths[iiCamera]}/capture_{now_str}.png'
+                                # img.save(filename)
+                                shutil.move(temp_image_name, filename)
+                                # print(f'saved file {filename}')
+                                print('.', end='')
+                    print(' ')
+
+            start_time = time.time()
--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 SCREEN_SIZE = (1920, 1080)
 VIDEO_BASEPATH = f'C:\\Users\\dporrat\\Desktop\\Netivei\\videos'
@@ -10,10 +11,17 @@ if not os.path.exists(VIDEO_BASEPATH):
 
 TIME_BETWEEN_FRAMES_SEC = 0.2
 
-CAMERA_LINKS = {'Abu_Gosh': 'https://www.iroads.co.il/%D7%AA%D7%99%D7%A7%D7%99%D7%99%D7%AA-%D7%9E%D7%A6%D7%9C%D7%9E'
-                            '%D7%95%D7%AA/#camera1Modal275-381',
-                'Raanana_Merkaz': 'https://www.iroads.co.il/%D7%AA%D7%99%D7%A7%D7%99%D7%99%D7%AA-%D7%9E%D7%A6%D7%9C'
-                                  '%D7%9E%D7%95%D7%AA/#camera1Modal188-43',
-                }
+if 0:
+    CAMERA_SEARCH_STRING = {'Abu_Gosh': 'אבו גוש',
+                            'Raanana_Merkaz': 'רעננה מרכז',
+                            'Aluf_Sadeh': 'אלוף שדה'}
 
-CAMERA = 'Raanana_Merkaz'
+CAMERAS = pd.DataFrame({'name': ['Raanana_Merkaz', 'Aluf_Sadeh'],
+                       'search_string': ['רעננה מרכז', 'אלוף שדה']})
+
+CAMERAS.set_index('name', inplace=True)
+
+CAMERA_URL = 'https://www.iroads.co.il/%D7%AA%D7%99%D7%A7%D7%99%D7%99%D7%AA-%D7%9E%D7%A6%D7%9C%D7%9E%D7%95%D7%AA/'
+CAMERA = 'Aluf_Sadeh'
+
+VIDEO_RESET_MIN = 0.5
