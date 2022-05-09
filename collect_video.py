df4e6a3db8fe3b92b633546c6f6d3e726dd391fe
Dana Porrat <dporrat@iai.co.il>
2022-05-09 14:23:30 +0300

    update

--- collect_video.py
+++ collect_video.py
@@ -28,6 +28,8 @@ def collect_one_camera(camera_name_):
     temp_image_name = f'screenshot_{camera_name_}.png'
     driver = None
     while True:
+        driver_error = False
+
         if driver is not None:
             try:
                 driver.close()
@@ -44,8 +46,11 @@ def collect_one_camera(camera_name_):
             driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
 
         driver.maximize_window()
-        driver.get(CAMERA_URL)
-        driver_error = False
+        try:
+            driver.get(CAMERA_URL)
+        except:
+            driver_error = True
+
         start_time = time.time()
         while not driver_error:
             while (time.time() < start_time + VIDEO_RESET_MIN * 60) and (not driver_error):
@@ -70,45 +75,50 @@ def collect_one_camera(camera_name_):
                     driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2)")
                     camera_element = locate_element(driver, "//li[@class='col-md-4 cam-item']")
                     if camera_element is None:
-                        raise ValueError(f'Cannot find camera link for {camera_name_}')
-
-                    camera_element.click()
-
-                    time.sleep(10)
-                    video_path = VIDEO_BASEPATH + OS_SEPARATOR + camera_name_
-                    if not os.path.exists(video_path):
-                        os.mkdir(video_path)
-
-                    # save images
-                    try:
-                        first_image_checked = False
-                        ii_file = 0
-                        restart_browser = False
-                        prev_time_sec = time.time()
-                        while (time.time() < start_time + VIDEO_RESET_MIN * 60) and (not restart_browser):
-                            time_elapsed_sec = time.time() - prev_time_sec
-                            if time_elapsed_sec >= TIME_BETWEEN_FRAMES_SEC:
-                                img = driver.save_screenshot(temp_image_name)
-                                now_utc = datetime.datetime.now(datetime.timezone.utc)
-                                now_str = now_utc.strftime('%Y_%m_%d_%H_%M_%S_%f')
-                                filename = video_path + OS_SEPARATOR + f'capture_{now_str}.png'
-                                shutil.copyfile(temp_image_name, filename)
-                                print('.', end='')
-                                ii_file += 1
-                                if ii_file > 30:
-                                    print('')
-                                    ii_file = 0
-                                if not first_image_checked:
-                                    if 0:
-                                        filename = r'/media/dana/second local disk1/dana/Netivei/videos/Aluf_Sadeh' + OS_SEPARATOR + 'capture_2022_02_02_12_17_32_733200.png'
-                                        camera_name_ = 'Aluf_Sadeh'
-                                    stam, image_ok, image_status = preprocess_one_image(temp_image_name, camera_name_, skip_time_test=True)
-                                    first_image_checked = True
-                                    restart_browser = image_status['paused'] or image_status['error'] or not image_status['camera_name_ok']
-
-                        print(' ')
-                    except:
                         driver_error = True
+
+                    if not driver_error:
+                        try:
+                            camera_element.click()
+                        except:
+                            driver_error = True
+
+                        if not driver_error:
+                            time.sleep(10)
+                            video_path = VIDEO_BASEPATH + OS_SEPARATOR + camera_name_
+                            if not os.path.exists(video_path):
+                                os.mkdir(video_path)
+
+                            # save images
+                            try:
+                                first_image_checked = False
+                                ii_file = 0
+                                restart_browser = False
+                                prev_time_sec = time.time()
+                                while (time.time() < start_time + VIDEO_RESET_MIN * 60) and (not restart_browser):
+                                    time_elapsed_sec = time.time() - prev_time_sec
+                                    if time_elapsed_sec >= TIME_BETWEEN_FRAMES_SEC:
+                                        img = driver.save_screenshot(temp_image_name)
+                                        now_utc = datetime.datetime.now(datetime.timezone.utc)
+                                        now_str = now_utc.strftime('%Y_%m_%d_%H_%M_%S_%f')
+                                        filename = video_path + OS_SEPARATOR + f'capture_{now_str}.png'
+                                        shutil.copyfile(temp_image_name, filename)
+                                        print('.', end='')
+                                        ii_file += 1
+                                        if ii_file > 30:
+                                            print('')
+                                            ii_file = 0
+                                        if not first_image_checked:
+                                            if 0:
+                                                filename = r'/media/dana/second local disk1/dana/Netivei/videos/Aluf_Sadeh' + OS_SEPARATOR + 'capture_2022_02_02_12_17_32_733200.png'
+                                                camera_name_ = 'Aluf_Sadeh'
+                                            stam, image_ok, image_status = preprocess_one_image(temp_image_name, camera_name_, skip_time_test=True)
+                                            first_image_checked = True
+                                            restart_browser = image_status['paused'] or image_status['error'] or not image_status['camera_name_ok']
+
+                                print(' ')
+                            except:
+                                driver_error = True
 
                 start_time = time.time()
 
